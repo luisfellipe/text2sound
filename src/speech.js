ab1a14df66d2e1bb28f2df18e4c42d924e870031
Luís Felipe <felipealvesoares@gmail.com>
2021-02-19 23:23:47 -0300

    nothing to worry about

--- src/speech.js
+++ src/speech.js
@@ -7,9 +7,9 @@ const TextToSpeechV1 = require('ibm-watson/text-to-speech/v1');
 const { IamAuthenticator } = require('ibm-watson/auth');
 
 // api access key
-const key = '6KLWMIcQh9GzY0TinudmsaqFSEZbr4yOceQzmYENbeJY';
+const key = '1QbJ2ni3XCPnfZ8q4n-zV3g5iBwXYN2QUKdqVW4TpHuu';
 // api acess url
-const url = 'https://api.us-south.text-to-speech.watson.cloud.ibm.com/instances/414a992a-6f0f-40b5-8420-b0853f467036';
+const url = 'https://api.us-south.text-to-speech.watson.cloud.ibm.com/instances/a942a5b4-6bfc-484f-8ea3-7414f501bb80';
 
 /**
  * textToSpeech 
@@ -31,7 +31,7 @@ function getAudio(comment){
   
   var params = {
     text: comment,
-    voice: "pt-BR_IsabelaV3Voice",
+    voice: "pt-BR_IsabelaV3Voice",//englis speech -> "en-US_AllisonVoice",
     accept:'audio/wav'
   };
   // synthesizes text in audio
